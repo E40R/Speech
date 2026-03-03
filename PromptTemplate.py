194ae74d05dcd8322becfb227385ae81d85a44b3
Kiriti <119531450+E40R@users.noreply.github.com>
2026-03-04 01:26:44 +0530

    Second LLM input

--- PromptTemplate.py
+++ PromptTemplate.py
@@ -5,28 +5,16 @@ from Services.triage_service import TriageService
 parser = PydanticOutputParser(pydantic_object=TriageService)
 
 # System Message
-system_message = """ 
-You=triage assistant, Do NOT diagnose.Always return valid JSON only.
-"""
+system_message = """ You=triage assistant, Do NOT diagnose.Always return valid JSON only. If human input is non health concern conversation then just rewrite exact input in key_details without changing."""
 
 
 # Create prompt template
 prompt = ChatPromptTemplate(
-    [
-        ("system", system_message),
-        ("human", """{user_input} {format_instructions}"""),
-    ],
-    partial_variables={
-        "format_instructions": parser.get_format_instructions()
-    }
+    [("system", system_message), ("human", """input:{user_input}. parser:{format_instructions}""")],
+    partial_variables={"format_instructions": parser.get_format_instructions()}
 )
 
-prompt2= ChatPromptTemplate(
-    [
-        ("system", system_message),
-        ("human", """{user_input}{triage} {format_instructions}"""),
-    ],
-    partial_variables={
-        "format_instructions": parser.get_format_instructions()
-    }
+tts_prompt = ChatPromptTemplate(
+    [("system","""You are a calm veterinary assistant speaking to a pet owner. Generate a short conversational response suitable for text-to-speech TTS. Rules: - Do NOT diagnose. - Do NOT change risk level. - Do NOT add new medical reasoning.- Only convert structured triage data into natural, reassuring speech. - If risk_level is emergency, emphasize urgency clearly but calmly."""),
+     ("human", """{triage_input}.""")]
 )
