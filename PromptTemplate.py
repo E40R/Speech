14d104f70ab210bb00db02dc8d8836bdd3c32f2e
Kiriti <119531450+E40R@users.noreply.github.com>
2026-03-14 00:33:07 +0530

    Refine prompt templates and system messages
    Updated system message and TTS prompt rules for better context handling and response generation.

--- PromptTemplate.py
+++ PromptTemplate.py
@@ -5,16 +5,32 @@ from Services.triage_service import TriageService
 parser = PydanticOutputParser(pydantic_object=TriageService)
 
 # System Message
-system_message = """ You=triage assistant, Do NOT diagnose.Always return valid JSON only. If human input is non health concern conversation then just rewrite exact input in key_details without changing."""
+system_message = """ 
+You=triage assistant, Do NOT diagnose.Always return valid JSON only. 
+If previous context exists, use it to maintain continuity: {previous_context}
+If human input is non health concern conversation then just store exact input in key_details without changing.
+"""
 
 
 # Create prompt template
 prompt = ChatPromptTemplate(
-    [("system", system_message), ("human", """input:{user_input}. parser:{format_instructions}""")],
-    partial_variables={"format_instructions": parser.get_format_instructions()}
+    [
+        ("system", system_message),
+        ("human", """input:{user_input}. parser:{format_instructions}"""),
+    ],
+    partial_variables={
+        "format_instructions": parser.get_format_instructions()
+    }
 )
 
 tts_prompt = ChatPromptTemplate(
-    [("system","""You are a calm veterinary assistant speaking to a pet owner. Generate a short conversational response suitable for text-to-speech TTS. Rules: - Do NOT diagnose. - Do NOT change risk level. - Do NOT add new medical reasoning.- Only convert structured triage data into natural, reassuring speech. - If risk_level is emergency, emphasize urgency clearly but calmly."""),
-     ("human", """{triage_input}.""")]
+    [
+        ("system","""You are a calm veterinary assistant speaking to a pet owner. Generate a conversational response suitable for TTS. 
+        Rules:- Do NOT diagnose/definitive diagnoses. 
+        -Include disclaimers about not replacing professional vet care. - Do NOT add or change anything new.
+        - Only convert structured triage data into natural, reassuring speech.
+        - If triage has conversation input then give polite and short response like "hey how are you?!" and try to keep response small and avoid disclaimers in only if 'converational input' else give the disclaimer.  
+         - If risk_level is emergency, emphasize urgency clearly."""),
+        ("human", """{triage_input}.""")
+    ]
 )
